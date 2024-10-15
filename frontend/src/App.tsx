86523a409e3268a246f0b16af1d82ef4269bfbe6
RenanTheFato <renanzintestes@gmail.com>
2024-10-14 23:30:35 -0300

    create the visual part of the registration page and format the codes, then integrate the login and registration pages with the API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home';
 import LoginForm from './pages/login';
+import SingUpForm from './pages/singup';
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/singin' element={<LoginForm/>} />
+        <Route path='/singup' element={<SingUpForm/>} />
       </Routes>
     </BrowserRouter>
     </>
--- frontend/src/pages/singup.tsx
+++ frontend/src/pages/singup.tsx
@@ -1,12 +1,13 @@
 /// <reference types="vite-plugin-svgr/client" />
 import { useState } from "react";
 import Logo from "../public/logo.svg?react";
+import { FaUser } from "react-icons/fa6";
 import { FiMail } from "react-icons/fi";
 import { IoMdLock } from "react-icons/io";
 import { MdLightMode } from "react-icons/md";
 import { FaMoon } from "react-icons/fa";
 
-function LoginForm() {
+function SingUpForm() {
   const [darkMode, setDarkMode] = useState(false);
 
   function toggleTheme() {
@@ -17,7 +18,7 @@ function LoginForm() {
     <>
       <div className={`${darkMode && "dark"}`}>
         <div
-          className={`w-full min-h-screen bg-off-white flex justify-center transition-colors duration-200 dark:bg-carbon-black`}
+          className='w-full min-h-screen bg-off-white flex justify-center transition-colors duration-200 dark:bg-carbon-black'
         >
           <div className="right-6 absolute my-4">
             <button
@@ -27,20 +28,28 @@ function LoginForm() {
               {darkMode ? <MdLightMode /> : <FaMoon />}
             </button>
           </div>
-          <main className="w-full m-10 text-white flex flex-col items-center justify-center">
+          <main className="w-full mx-10 my-4 text-white flex flex-col items-center justify-center">
             <div className="w-full flex justify-center">
               <Logo className="fill-carbon-black w-32 h-32 justify-center items-center 2xl:w-48 2xl:h-48 dark:fill-off-white" />
             </div>
 
             <div className="w-full max-h-[80vh] bg-[#D9D9D9] bg-opacity-30 my-2 rounded-lg shadow-signature sm:w-96 2xl:w-[28rem] dark:shadow-azure-radiance-600">
-              <form className="p-4">
+              <form className="p-4 overflow-hidden">
                 <div className="w-full h-10 flex justify-center items-center">
                   <h1 className="text-4xl font-outfit font-semibold text-carbon-black 2xl:text-5xl 2xl:mt-6 dark:text-off-white">
-                    Login
+                    Cadastro
                   </h1>
                 </div>
 
-                <div className="w-full h-full flex flex-col space-y-10 my-5 2xl:mt-10 2xl:space-y-12">
+                <div className="w-full h-full flex flex-col space-y-6 my-5 2xl:mt-10 2xl:space-y-12">
+                  <div className="w-full flex flex-row relative">
+                    <FaUser className="text-carbon-black absolute text-xl top-2.5 mx-1 2xl:text-2xl dark:text-off-white" />
+                    <input
+                      type="text"
+                      className="w-full p-2 px-8 bg-transparent text-carbon-black font-outfit outline-none border-b-carbon-black border-b-2 placeholder:text-carbon-black 2xl:text-xl dark:text-off-white dark:border-b-off-white dark:placeholder:text-off-white"
+                      placeholder="Insira seu nome de usuário..."
+                    />
+                  </div>
                   <div className="w-full flex flex-row relative">
                     <FiMail className="text-carbon-black absolute text-xl top-2.5 mx-1 2xl:text-2xl dark:text-off-white" />
                     <input
@@ -54,24 +63,24 @@ function LoginForm() {
                     <input
                       type="password"
                       className="w-full p-2 px-8 bg-transparent text-carbon-black font-outfit outline-none border-b-carbon-black border-b-2 placeholder:text-carbon-black 2xl:text-xl dark:text-off-white dark:border-b-off-white dark:placeholder:text-off-white"
-                      placeholder="Insira sua senha..."
+                      placeholder="Crie sua senha..."
                     />
                   </div>
                 </div>
 
                 <button
                   type="submit"
-                  className="bg-bay-of-many-900 w-full p-2 mb-3 mt-3 rounded-lg font-outfit text-lg 2xl:text-xl 2xl:my-4"
+                  className="bg-bay-of-many-900 w-full p-2 mb-4 mt-2 rounded-lg font-outfit text-lg 2xl:text-xl 2xl:my-4"
                 >
-                  Acessar
+                  Cadastrar
                 </button>
 
                 <div className="w-full my-2 flex justify-between items-center">
                   <span className="text-carbon-black 2xl:text-lg dark:text-off-white">
-                    Não tem uma conta?
+                    Já tem uma conta?
                   </span>
                   <span className="text-azure-radiance-900 hover:cursor-pointer 2xl:text-lg dark:text-azure-radiance-300">
-                    Registre-se agora!
+                    Faça login!
                   </span>
                 </div>
               </form>
@@ -82,5 +91,4 @@ function LoginForm() {
     </>
   );
 }
-
-export default LoginForm;
+export default SingUpForm;
